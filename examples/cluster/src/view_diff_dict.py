49157468a0cb4993d81021d02503f9d943b6fb55
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2022-06-24 12:09:47 +0200

    added file to view diff dict

--- examples/cluster/src/view_diff_dict.py
+++ examples/cluster/src/view_diff_dict.py
@@ -24,8 +24,8 @@ try:
     with open(path, "rb") as f:
         loaded = pickle.load(f)
         for i, (k,v) in enumerate(loaded.items()):
-            print("{} \t {}".format(k, v))
+            print("\n{}\n1: {} \n2: {}\n".format(k, v["1"], v["2"]))
             if (i+1) > number:
-                break;
+                break
 except:
     raise "File cannot be read: {}".format(path)
